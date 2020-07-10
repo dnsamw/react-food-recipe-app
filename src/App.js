cdc043ffa8bbde7f26f44465e87cf250dbc85320
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-07-11 00:24:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
+
+const App = () => {
+  const APP_ID = "9a963140";
+  const APP_KEY = "0beeb79d38522c5dca11445bde9f026b";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const exampleReq = `https://edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(proxyurl + exampleReq);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default App;
